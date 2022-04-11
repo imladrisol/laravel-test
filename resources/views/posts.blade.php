19901ce7e4204c9afd0b1733fc6a17b69a87e0b3
Olga Nevinchana <nevinchana@rozetka.com.ua>
2022-04-11 11:42:56 +0300

    31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,17 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1>
-                <a href="post/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
+@include('_posts-header')
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card />
 
-                <p>
-                    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in  <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
-                </p>
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card />
+        <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+    </div>
+</main>
 </x-layout>
